a274e4cdc93c59c9bec428daf41fb1be77bcbb5a
Fiachra O' Donoghue <fiachra.m.odonoghue@gmail.com>
2021-02-05 02:42:41 +0000

    started week3 task - secondstring.py

---
+++ secondstring.py
@@ -0,0 +1,8 @@
+# secondstring.py
+# A program that asks the user to enter a string and prints out 
+# every second letter of that string in reverse order
+# Author: Fiachra O' Donoghue
+
+# Ask the user to enter a string and assign it to the variable sentence
+sentence = input("Please enter a sentence: ")
+

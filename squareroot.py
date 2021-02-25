9c410f6621d1e0fede523b250ca68261d577975a
Fiachra O' Donoghue <fiachra.m.odonoghue@gmail.com>
2021-02-25 16:06:27 +0000

    refactoring squareroot.py

--- squareroot.py
+++ squareroot.py
@@ -2,37 +2,61 @@
 # Estimate the square root of the provided number
 # Author: Fiachra O' Donoghue
 
+# This program uses Newton's method to approximate square roots
+# The description of Newton's method in the paper below was used to develop the program
 # https://www2.math.upenn.edu/~kazdan/202F09/sqrt.pdf
+
 # To approximate the square root, x, of a real number A, we need to solve 
 # x**2 - A = 0
-# So given a real number A, we estimate the square root, x and calculate x**2 - A
-# If the result is greater than 0 then our estimate must be too high, 
-# and if it is less than 0 then our estimate must be too low
-# If it is too high e should half it and try again, and if it is too low we should
-# increase to half way between the current estimate and the last one
+# So given a real number A, we estimate the square root, x and calculate (x**2 - A)
+# If the result is 0 then we have found the square root
+# If the result is not 0 nor within an accptable margin of error from zero then a new 
+# and better estimate can be calculated as:
+# new_x = (x + (A / x)) / 2  
+# (remember that x is the current estimate and A is the number for which the square root 
+# is sought)
 
 
-def sqrt(a):
+# The sqrt function takes a number for which the square root will be estimated
+# and an optional error limit at which it will stop seeking a better estimate
+# The limit defaults to 0.0000000001. This was chosen arbitrarily but tested with
+# a wide variety of input data
+def sqrt(num, limit = 0.0000000001):
 
-    # The first estimate should be such that its square is greater than a
-    # https://www2.math.upenn.edu/~kazdan/202F09/sqrt.pdf
-    estimate = 0
+    # The first estimate should be such that its square is greater than A
+    # (see https://www2.math.upenn.edu/~kazdan/202F09/sqrt.pdf)
+
+    # If the A is less than 1 then 1 is a good first estimate
+    # because its square guaranteed to be bigger than A
+    estimate = 1
     
-    if a > 1:
-        estimate = a / 2
+    # and if A is greater than 1 than A/2 is a good first estimate because, again
+    # its square is guaranteed to be bigger than A
+    if num > 1:
+        estimate = num / 2
 
-    result = a
+    # error is the variable that will hold the result of the test calculation
+    # (x**2 - A) on each iteration. It must be declared outside the loop as 
+    # it provides the stopping condition for the while loop
+    error = num
 
-    # We must use the absolute result as sometimes the result of x**2 - a < 0 
-    while abs(result) > 0.0000000001:
+    # We must use the absolute error as sometimes the result of x**2 - a < 0 
+    while abs(error) > limit:
 
-        result = estimate**2 - a
+        # Test the current estimate
+        error = estimate**2 - num
 
-        estimate = (estimate + (a/estimate))/2
- 
-        print("Result: {}\tEstimate: {}".format(result, estimate))
+        # Calculate the new estimate
+        estimate = (estimate + (num / estimate)) / 2
 
+    # Return the approximated square root
     return(estimate)
     
-#for i in range(100):
-print(sqrt(2))
+# Obtain number from user
+num = float(input("Please enter a positive number: "))
+
+# Approximate the square root
+result = sqrt(num)
+
+# Print the result, rounded to 1 decimal place
+print("The square root of {} is approx. {:.1f}".format(num, result))

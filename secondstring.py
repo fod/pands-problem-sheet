fa22844a871a7021b39285c48ada9bb488d92994
Fiachra O' Donoghue <fiachra.m.odonoghue@gmail.com>
2021-03-02 01:29:21 +0000

    improved comments in secondstring.py

--- secondstring.py
+++ secondstring.py
@@ -6,7 +6,13 @@
 # Ask the user to enter a string and assign it to the variable sentence
 sentence = input("Please enter a sentence: ")
 
-# take a slice from the string, starting at the end and taking every second character
+# Take a slice from the string, starting at the end and taking every second character.
+# The empty space before the first colon indicates that the slice should start at 
+# the beginning of the string, the empty space before the second colon indicates that the 
+# slice should run to the end of the string, the third argument is the step: the fact that 
+# it is negative indicates that the slice should start at the end of the string and run 
+# backwards through the string, and the 2 indicates that every second character should be 
+# included
 output = sentence[::-2]
 
 # print the modified string to stdout

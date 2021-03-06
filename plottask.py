1998ed09f51ce217768324a115d695ac38a634dd
Fiachra O' Donoghue <fiachra.m.odonoghue@gmail.com>
2021-03-06 18:32:53 +0000

    initial plottask.py

---
+++ plottask.py
@@ -0,0 +1,11 @@
+# plottask.py
+# Produce plot of linear, quadratic, and cubic functions: 
+# f(x) = x, f(x) = x^2 and f(x) = x^3
+# Author: Fiachra O' Donoghue
+
+# import the matplotlib.pyplot library and alias to plt
+import matplotlib.pyplot as plt
+
+# import numpy array manipulation library and alias to np
+import numpy as np
+
